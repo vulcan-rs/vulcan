f84f6e489858ce036cf512b9351d1f54f5b83df3
Techassi <git@techassi.dev>
2022-12-12 17:49:02 +0100

    Add and rename constants

--- libs/dhcp/src/constants.rs
+++ libs/dhcp/src/constants.rs
@@ -1,6 +1,8 @@
 pub const MIN_DHCP_MSG_SIZE: usize = 300;
-pub const BOOTP_OPCODE_REQUEST: u8 = 1;
-pub const BOOTP_OPCODE_REPLY: u8 = 2;
+pub const DHCP_OPCODE_REQUEST: u8 = 1;
+pub const DHCP_OPCODE_REPLY: u8 = 2;
+pub const DHCP_MAGIC_COOKIE: u32 = 1_669_485_411;
+pub const DHCP_MAGIC_COOKIE_ARR: [u8; 4] = [99, 130, 83, 99];
 
 pub const HARDWARE_ADDR_TYPE_ETHERNET: u8 = 1;
 pub const HARDWARE_ADDR_LEN_ETHERNET: u8 = 6;
