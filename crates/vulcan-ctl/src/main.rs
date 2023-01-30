7db92323a6eb34650c7c6ab3d3bbed7e34d79f24
Techassi <git@techassi.dev>
2023-01-30 22:17:11 +0100

    Delete unused code

--- crates/vulcan-ctl/src/main.rs
+++ crates/vulcan-ctl/src/main.rs
@@ -1,15 +1,1 @@
-mod repl;
-
-use repl::Repl;
-
-fn main() {
-    let mut repl = Repl::new(">> ");
-
-    match repl.run() {
-        Ok(_) => {}
-        Err(err) => {
-            println!("{}", err);
-            std::process::exit(1);
-        }
-    }
-}
+fn main() {}
