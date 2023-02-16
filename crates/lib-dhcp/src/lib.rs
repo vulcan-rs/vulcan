6ce77c260d9defb79f51ef79bb1eaa4bdbe7a7f8
Techassi <git@techassi.dev>
2023-02-16 14:46:52 +0100

    Add utils::timeout function

--- crates/lib-dhcp/src/lib.rs
+++ crates/lib-dhcp/src/lib.rs
@@ -5,9 +5,11 @@ mod constants;
 mod error;
 mod server;
 mod storage;
+mod utils;
 
 pub use client::*;
 pub use constants::*;
 pub use error::*;
 pub use server::*;
 pub use storage::*;
+pub use utils::*;
